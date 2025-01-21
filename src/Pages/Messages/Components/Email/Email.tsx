bae5f509b69afb3c542ae2cf39ea759af5a87ae1
Rutik Kamble <kamblerutik155@gmail.com>
2025-01-21 21:12:11 +0530

    API integreated in support page

--- src/Pages/Messages/Components/Email/Email.tsx
+++ src/Pages/Messages/Components/Email/Email.tsx
@@ -3,25 +3,48 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 
-interface Message {
-  date: string;
-  message: string;
-  userName: string;
+interface Admin {
+  email: string;
+  fullName: string;
+  _id: string;
+}
+
+interface User {
+  email: string;
+  name: string;
+  phoneNumber: string;
+  _id: string;
+}
+
+interface Complaints {
+  attachments: string[];
+  createdAt: string;
+  description: string;
+  resolutionNotes: string;
+  resolvedAt: string;
+  resolvedBy: Admin;
+  status: "Pending" | "Resolved" | "Rejected";
+  subject: string;
+  ticketId: string;
+  updatedAt: string;
+  user: User;
+  __v: string;
+  _id: string;
 }
 
 const Email = () => {
-  const [messages, setMessages] = useState<Message[]>([]);
+  const [complaints, setComplaints] = useState<Complaints[]>([]);
+
   const token = Cookies.get("token");
 
   const getContactUsMessages = async () => {
     try {
-      const response = await axios.get(`${baseURL}/user/contact-us-messages`, {
+      const response = await axios.get(`${baseURL}/complaints`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
-      console.log(response.data);
-      setMessages(response.data);
+      setComplaints(response.data.complaints);
     } catch (error) {
       console.log(error);
     }
@@ -41,24 +64,49 @@ const Email = () => {
           <td>Status</td>
           <td>Action</td>
         </tr>
-        {messages.map((message, i) => (
+        {complaints.map((complaint, i) => (
           <tr
             key={i}
             className=" hover:bg-[#242424] border-b transition-all duration-300 cursor-pointer rounded-md "
           >
             <td>
-              <h2 className="p-2  rounded-sm font-normal  w-56 text-nowrap overflow-hidden">
-                {message.userName.length > 10
-                  ? `${message.userName.slice(0, 10)}...`
-                  : message.userName}
+              <h2 className="p-2 flex flex-col">
+                <span className="rounded-sm font-normal  w-56 text-nowrap overflow-hidden">
+                  # {complaint.ticketId}
+                </span>
+                <span className="  rounded-sm font-extralight  w-56 text-nowrap overflow-hidden">
+                  {complaint.user.name}
+                </span>
               </h2>
             </td>
             <td>
-              {message.message.length > 50
-                ? `${message.message.slice(0, 50)}...`
-                : message.message}
+              <h2 className="p-2 flex flex-col">
+                <span className="rounded-sm font-normal  w-56 text-nowrap overflow-hidden">
+                  {complaint.user.email}
+                </span>
+                <span className="  rounded-sm font-extralight  w-56 text-nowrap overflow-hidden">
+                  +91 {complaint.user.phoneNumber}
+                </span>
+              </h2>
             </td>
-            <td className="text-left">{message.date}</td>
+            <td>
+              <h2 className="p-2  rounded-sm font-normal  w-56 text-nowrap overflow-hidden">
+                {complaint.subject}
+              </h2>
+            </td>
+            <td>
+              <h2
+                className={`p-2 rounded-md font-medium  w-fit text-nowrap overflow-hidden
+                ${
+                  complaint.status === "Resolved"
+                    ? "bg-[#F0FFF8] text-[#18AB56]"
+                    : "bg-[#FFF0F0] text-[#FFBC10]"
+                }
+                `}
+              >
+                {complaint.status}
+              </h2>
+            </td>
           </tr>
         ))}
       </table>
--- src/Pages/Messages/Components/Header/Header.tsx
+++ src/Pages/Messages/Components/Header/Header.tsx
@@ -3,39 +3,104 @@ import { IoChatboxSharp } from "react-icons/io5";
 import { BsPersonFillCheck } from "react-icons/bs";
 import { BsPersonFillExclamation } from "react-icons/bs";
 import { BsPersonFillSlash } from "react-icons/bs";
+import { useEffect, useState } from "react";
+import { baseURL } from "@baseURL";
+import axios from "axios";
+import Cookies from "js-cookie";
+
+interface Admin {
+  email: string;
+  fullName: string;
+  _id: string;
+}
+
+interface User {
+  email: string;
+  name: string;
+  phoneNumber: string;
+  _id: string;
+}
+
+interface Complaints {
+  attachments: string[];
+  createdAt: string;
+  description: string;
+  resolutionNotes: string;
+  resolvedAt: string;
+  resolvedBy: Admin;
+  status: "Pending" | "Resolved" | "Rejected";
+  subject: string;
+  ticketId: string;
+  updatedAt: string;
+  user: User;
+  __v: string;
+  _id: string;
+}
 
 const Header = () => {
-  return (
-    <div className="flex w-full justify-center gap-8">
-      <SupportCard
-        icon={<IoChatboxSharp />}
-        text="Total Complaints"
-        count={3}
-        onClick={() => null}
-        width={4}
-      />
-      <SupportCard
-        icon={<BsPersonFillCheck />}
-        text="Total solved"
-        count={3}
-        onClick={() => null}
-        width={4}
-      />
+  const [complaints, setComplaints] = useState<Complaints[]>([]);
 
-      <SupportCard
-        icon={<BsPersonFillExclamation />}
-        text="Total unsolved"
-        count={3}
-        onClick={() => null}
-        width={4}
-      />
-      <SupportCard
-        icon={<BsPersonFillSlash />}
-        text="Total pending"
-        count={3}
-        onClick={() => null}
-        width={4}
-      />
+  const token = Cookies.get("token");
+
+  const getContactUsMessages = async () => {
+    try {
+      const response = await axios.get(`${baseURL}/complaints`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setComplaints(response.data.complaints);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getContactUsMessages();
+  }, []);
+
+  const supportCardData = [
+    {
+      icon: <IoChatboxSharp />,
+      text: "Total Complaints",
+      count: complaints.length,
+      onClick: () => console.log("Total Complaints clicked"),
+    },
+    {
+      icon: <BsPersonFillCheck />,
+      text: "Total Solved",
+      count: complaints.filter((complaint) => complaint.status === "Resolved")
+        .length,
+      onClick: () => console.log("Total Solved clicked"),
+    },
+    {
+      icon: <BsPersonFillExclamation />,
+      text: "Total Unsolved",
+      count: complaints.filter((complaint) => complaint.status === "Rejected")
+        .length,
+      onClick: () => console.log("Total Unsolved clicked"),
+    },
+    {
+      icon: <BsPersonFillSlash />,
+      text: "Total Pending",
+      count: complaints.filter((complaint) => complaint.status === "Pending")
+        .length,
+      onClick: () => console.log("Total Pending clicked"),
+    },
+  ];
+
+  return (
+    <div style={{ display: "flex", gap: "16px" }}>
+      {supportCardData.map((card, index) => (
+        <SupportCard
+          key={index}
+          icon={card.icon}
+          text={card.text}
+          count={card.count}
+          onClick={card.onClick}
+          width={4}
+        />
+      ))}
     </div>
   );
 };
