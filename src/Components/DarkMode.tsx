a004bb1aa8d506035ee3546084897a68657005a4
Rutik Kamble <kamblerutik155@gmail.com>
2025-01-30 21:24:10 +0530

    minor changes

--- src/Components/DarkMode.tsx
+++ src/Components/DarkMode.tsx
@@ -1,15 +1,24 @@
-import { useDarkMode } from "../context/DarkModeContext";
-import { MdWbSunny } from "react-icons/md";
 import { IoMoonSharp } from "react-icons/io5";
+import { MdWbSunny } from "react-icons/md";
+import Switch from "react-switch";
+import { useDarkMode } from "../context/DarkModeContext";
 
 const DarkMode = () => {
   const { darkMode, toggleDarkMode } = useDarkMode();
 
+  console.log(darkMode);
+
   return (
     <button
-      onClick={toggleDarkMode}
-      className="p-2 rounded absolute text-3xl right-10 bg-[#111] dark:bg-[#fff]"
+      // onClick={toggleDarkMode}
+      className="p-2 rounded absolute text-3xl flex gap-2 right-10 bg-[#111] dark:bg-[#fff]"
     >
+      <Switch
+        onChange={toggleDarkMode}
+        checked={darkMode}
+        uncheckedIcon={false}
+        checkedIcon={false}
+      />
       {darkMode ? <IoMoonSharp /> : <MdWbSunny />}
     </button>
   );
